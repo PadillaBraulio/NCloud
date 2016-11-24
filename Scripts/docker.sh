98574b8a6c96ee6df07a3533d127a501aa436161
PadillaBraulio <brauliojuancarlos@gmail.com>
2016-11-24 16:47:31 -0600

    Update docker.sh

--- Scripts/docker.sh
+++ Scripts/docker.sh
@@ -7,6 +7,7 @@ sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-keys 58$
 echo deb https://apt.dockerproject.org/repo ubuntu-xenial main | sudo tee /etc/$
 sudo apt-get update
 sudo apt-get install -y docker-engine
+sudo apt-get install -y docker.io
 sudo service docker start
 
 
