74fc8ef8f897fc553983f2b95b0cf69a045fbee8
PadillaBraulio <brauliojuancarlos@gmail.com>
2016-11-30 09:07:34 -0600

    Update apache.sh

--- Scripts/apache.sh
+++ Scripts/apache.sh
@@ -3,3 +3,6 @@
 sudo apt-get install apache2 -y
 sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql -y
 sudo apt-get install php7.0-curl -y
+sudo wget https://github.com/kelseyhightower/confd/releases/download/v0.11.0/confd-0.11.0-linux-amd64
+sudo cp confd-0.11.0-linux-amd64 /usr/local/bin/confd
+sudo chmod 777 /usr/local/bin/confd
